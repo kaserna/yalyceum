e2015a079e38a0d739bc7480be4a9dfaecca0b0d
Andy <andy813777@gmail.com>
2023-10-06 22:08:44 +0300

    Л1 Задача 2

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -10,24 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from utils import load_env 
+
+get_env = os.environ.get
+BASE_DIR = Path(__file__).parent.parent.parent 
+load_env(BASE_DIR / "../.env")
+SECRET_KEY = get_env("DJANGO_SECRET_KEY", "secret")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = get_env("DEBUG", False)
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-@_a@^iv_b*_gf+hdt_a6x*-g2n6pm#v9_gmv8^==5i^2c)$dzf"
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = get_env("ALLOWED_HOSTS", [])
 
 
 # Application definition
